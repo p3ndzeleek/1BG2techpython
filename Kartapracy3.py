81c1928b1617d2717c5297f6380a5eca5fecb3da
1B1PB <114579263+p3ndzeleek@users.noreply.github.com>
2022-10-23 13:18:42 +0000

    jd

--- Kartapracy3.py
+++ Kartapracy3.py
@@ -47,10 +47,26 @@
 # for i in range(10):
 #     print(i**2, end=" ")
 
-n = int(input())
-for i in range(1, n+1):
-    if n % i ==0:
-        print(n, end=" ")
+# n = int(input())
+# for i in range(1, n+1):
+#     if n % i ==0:
+#         print(n, end=" ")
 
+# suma = 0
+# for i in range(10, 100):
+#     suma = suma + i
+# print(suma)
+
+# suma = 0
+# for i in range(100, 1000, 2):
+#     suma = suma + i
+# print(suma)
+
+liczba = 0
+a = int(input("podaj liczbe graczy"))
+for 
+
+for i in range(1, a):
 
     
+
---
+++ kartkowka.py
@@ -0,0 +1,6 @@
+a, g =int(input()), int(input())
+if (a+g)/2 > (a*g)**2:
+    print("Tak")
+else:
+    print("Nie")
+    

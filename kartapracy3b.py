278d4f28c0428e000b46aa0725fe72e3755368a6
1B1PB <114579263+p3ndzeleek@users.noreply.github.com>
2022-10-23 14:42:02 +0000

    do 4zad

---
+++ kartapracy3b.py
@@ -0,0 +1,33 @@
+# #zad1 
+# for i in range(1,31):
+#     print(i, "dni")
+
+#zad2
+
+# for i in range(1, 10, 2):
+#     print(i**2, "no kwadraty ma sie rozumieć")
+
+#zad3
+
+# for i in range(1138, 9855):
+#     if i %379 ==0:
+#         print(i)
+
+#zad4
+
+# for i in range(101, 1000):
+#     if i %5 ==0 and i %6 ==0 and i %11 ==0:
+#         print(i)
+
+#zad5
+# skopiowane 
+# a = int(input("podaj ilośc liczb: "))
+# Liczby = []
+# for i in range(0, a):
+#     temp = int(input(f"Podaj {i+1} liczbę: "))
+#     Liczby.append(temp)
+# print(f"Wynik to: {sum(Liczby)}")
+
+
+
+

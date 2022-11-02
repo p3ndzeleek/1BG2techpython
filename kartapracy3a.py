63294c2d8adfa5a44cbaf8fbcbbd9fa82b51f2c0
1B1PB <114579263+p3ndzeleek@users.noreply.github.com>
2022-11-02 12:20:26 +0000

    powturka

--- kartapracy3a.py
+++ kartapracy3a.py
@@ -15,13 +15,21 @@
 #     else:
 #          print("--", end=" ")   
 
-n = int(input())
-znak = "--"
-for i in range(1, n+1):
-    print("*"*i, end=" ")
-    if znak == "--":
-        znak = "||"
-    else:
-        znak="--"
-        print(znak, end=" ")
+# n = int(input())
+# znak = "--"
+# for i in range(1, n+1):
+#     print("*"*i, end=" ")
+#     if znak == "--":
+#         znak = "||"
+#     else:
+#         znak="--"
+#         print(znak, end=" ")
+
+# n = int(input())
+# for i in range(1, n+1):
+#   print("*", end=" ")
+#   if i %2 ==0:
+#       print("-"*i, end=" ")
+# else:
+#     print("|"*i, end=" ")
 

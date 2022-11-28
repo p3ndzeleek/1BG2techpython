183ed57b520be858487ec362c5fc95eac3929e50
1B1PB <114579263+p3ndzeleek@users.noreply.github.com>
2022-11-28 12:06:03 +0000

    cos  z algorytmów

--- algorytmy1/1.py
+++ algorytmy1/1.py
@@ -1,6 +1,38 @@
-n = int(input())
-for i in range(2,n):
-    if n % i ==0:
-        print("nie jest pierwsza")
-        exit()
-print("liczba jest pierwsza")
+# # n = int(input())
+# # for i in range(2,n):
+# #     if n % i ==0:
+# #         print("nie jest pierwsza")
+# #         exit(0)
+# # print("liczba jest pierwsza")
+
+
+# # p, q = int(input()), int(input())
+# # for j in range(p, q+1):
+# #     flaga = True
+# #     for i in range(2,j):
+# #         if j % i ==0:
+# #             flaga = False
+# #             break
+# #     if flaga == True:
+# #         print(i, end=" ")
+
+
+# n = int(input("Podaj ile chcesz liczb pierwszych: "))
+
+# k = 2
+# ilosc = 0
+# while 1:
+#     flaga = True
+#     for i in range(2, k):
+#         if k % i == 0:
+#             flaga = False
+#             break
+#     if flaga == True:
+#         print(k, end=" ")
+#         ilosc = ilosc + 1
+#     if ilosc == n:
+#         break
+#     k = k + 1
+
+
+

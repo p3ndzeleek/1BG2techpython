8bd254ffa8e8fd1b50e712769b70649b4f54673c
1B1PB <114579263+p3ndzeleek@users.noreply.github.com>
2022-11-30 12:14:22 +0000

    nww nwd

---
+++ algorytmy1/nww.nwd.py
@@ -0,0 +1,16 @@
+# # #nwd
+# # a = int(input())
+# # b = int(input())
+# # iloraz = a * b
+# # while a != b:
+# #     if a > b : a = a - b
+# #     if b > a : b = b - a
+# # print(iloraz // a)
+
+
+# a = int(input())
+# b = int(input())
+# iloczyn = a * b
+# while b > 0:
+#     a ,b = b , a %b
+# print(iloczyn / a)

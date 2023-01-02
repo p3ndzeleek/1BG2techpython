76c5269bedf5d03661afa570191749e245b672af
1B1PB <114579263+p3ndzeleek@users.noreply.github.com>
2023-01-02 19:59:34 +0000

    poćwiczone

---
+++ mojeprace/cwiczenie.py
@@ -0,0 +1,87 @@
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if b-a == c-b:
+#     print("tak")
+
+# if b/a == c/b:
+#     print("tak")
+
+# if a<b<c:
+#     print("tak")
+# if a>b>c:
+#     print("nie")
+
+# suma = 0
+# for i in range(101,1000):
+#  if i %8 ==0 and i %16 !=0:
+#      suma += i
+# print(suma)
+
+
+# for i in range(11,100):
+#     if i %7 ==0:
+#         wielokrotnosc = 1
+# ilosc = 0 
+# for i in range(100,10000):
+#     if i % wielokrotnosc ==0:
+#         ilosc += 1
+# print(ilosc)
+    
+# ilosc = 0
+# for i in range(10, 100):
+#     if 2 * (i % 10) <= i // 10:
+#         ilosc += 1
+# print(ilosc)   
+
+# n = int(input())
+# suma = 0 
+# for i in range(1, n+1):
+#     il = 1
+#     for i in range(1,i+1):
+#         il = il * i
+#     suma += il
+# print(suma)
+
+
+# niebanalny 1
+
+# n = int(input())
+# a = 1
+# suma = 0
+# for i in range(1,n+1):
+#     b = i*i
+#     suma += a/b
+#     a += 2
+# print(suma)
+
+
+
+#niebalalny 2
+#n = int(input())
+# a = 1
+# suma = 0
+# sumb = 0
+# for i in range(1, n + 1):
+#     suma += a
+#     a += 2
+#     b = i * i
+#     sumb += b
+# print(suma/sumb)
+
+
+
+
+
+
+#ciąg wymagający 
+#n = int(input())
+# a = 2
+# suma = 0
+# for i in range(1, n + 1):
+#     b = i ** 3 + 2
+#     suma += a/b
+#     a += 2
+# print(suma)
+
+

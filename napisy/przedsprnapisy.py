d0212c96ec0a41faf853595d1ee6a31eeddaecf5
1B1PB <114579263+p3ndzeleek@users.noreply.github.com>
2023-03-29 11:20:48 +0000

    napisy przed spr

--- napisy/przedsprnapisy.py
+++ napisy/przedsprnapisy.py
@@ -18,10 +18,57 @@
 # print(b)
 
 #3
-c = input()
-l = list(c)
-l.reverse()
-c = "".join(l)
-print(c[:4])
+# c = input()
+# l = list(c)
+# l.reverse()
+# c = "".join(l)
+# print(c[:4])
 
+
+#4 średnia z liter 
+# d = input()
+# s = 0 
+# for x in d:
+#     s += ord(x)
+# znak = s // len(d)
+# print(chr(znak))
+
+
+#5 oblicznie ilości danego znaku np.a i ilośc samogłosek 
+
+# e = input()
+# ilosc = 0
+# for x in e:
+#     if x == "A":
+#         ilosc += 1
+# print(ilosc)
+
+# SAMO = ["A", "E","I","O","U","Y"]
+
+# ilosc_samo = 0
+# for x in e:
+#     if x in SAMO:
+#         ilosc_samo += 1
+# ilosc_samo
+
+#6 
+
+# f = input()
+# maksiu = 0
+# for x in f:
+#     if f.count(x) > maksiu:
+#         maksiu = f.count(x)
+#         literka = x
+# print(literka, maksiu)
+
+#8 
+h = input()
+ilosc = 0
+for i in range(len(h)):
+    if h[i:i+2] == "LA":
+        ilosc += 1
+if ilosc == 3:
+    print("taK")
+else:
+    print("NIe")
 
